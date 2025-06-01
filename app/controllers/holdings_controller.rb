1359a1920942e140d0c3f481140a0cb5604e4cdd
Alessandro Gerotto <alessandrogerotto@archeido.com>
2025-06-01 15:46:01 +0200

    feat: included price in the holdings

--- app/controllers/holdings_controller.rb
+++ app/controllers/holdings_controller.rb
@@ -1,22 +1,30 @@
 class HoldingsController < ApplicationController
   def index
-    holdings = Holding.all
+  holdings = Holding.all
 
-    render json: holdings.map { |h|
-      {
-        id: h.id,
-        category: h.category,
-        label: h.label,
-        quantity: h.quantity.to_f,
-        created_at: h.created_at,
-        updated_at: h.updated_at
-      }
+  # Prendo i simboli da label (crypto, ETF, ecc.)
+  symbols = holdings.map(&:label).uniq
+
+  # Prendo i prezzi per questi simboli
+  prices = CoinMarketCapFetcher.new.fetch_prices(symbols)
+
+  result = holdings.map do |h|
+    {
+      id: h.id,
+      category: h.category,
+      label: h.label,
+      quantity: h.quantity.to_f,
+      price: prices[h.label],  # usa label per cercare il prezzo
+      created_at: h.created_at,
+      updated_at: h.updated_at
     }
   end
 
+  render json: result
+end
+
   def get_price
     prices = CoinMarketCapFetcher.new.fetch_prices(['BTC', 'ETH', 'SOL', 'DOT', 'CRO']) 
-    Rails.logger.info "Prices: #{prices.inspect}"  # prices: {"BTC"=>104283.64293003875, "ETH"=>2494.499933147048, "SOL"=>152.08263797451366, "DOT"=>4.026426879903671, "CRO"=>0.10285899614457555}
     render json: prices.map { |symbol, price| { symbol: symbol, price: price } }
   end
 end
