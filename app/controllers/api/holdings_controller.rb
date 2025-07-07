17e297a6b264aa57977636e0d2f0daba6f60acf6
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-07-07 22:38:47 +0200

    fix: fetch_and_store_price_for

--- app/controllers/api/holdings_controller.rb
+++ app/controllers/api/holdings_controller.rb
@@ -52,13 +52,26 @@ module Api
       params.require(:holding).permit(%i[label quantity category])
     end
 
+    # Fetches the latest price for a given holding (only if it's a crypto asset)
+    # Queries the CoinMarketCap API only if necessary (smart caching).
+    #
+    # Creates a new Price with the holding field set to the given holding,
+    # which correctly assigns the holding_id foreign key.
     def fetch_and_store_price_for(holding)
-      if holding.category == 'crypto'
-        CoinMarketCapFetcher.new.fetch_prices(holding.label)
-      else
-        # TODO
-        Rails.logger.info "No fetch needed for ETF: #{holding.label}"
-      end
+      return unless holding.category == 'crypto' # TODO: Manage other assets
+
+      fetcher = CoinMarketCapFetcher.new
+      prices = fetcher.fetch_prices(holding.label)
+
+      return unless prices[holding.label].present?
+
+      Price.create!(
+        label: holding.label,
+        price: prices[holding.label],
+        category: holding.category,
+        retrieved_at: Time.current,
+        holding: holding
+      )
     end
   end
 end
