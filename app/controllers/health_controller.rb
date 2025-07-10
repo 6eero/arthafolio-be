07b190ceeacb01dc6e63f16b8d38e8688b06aef7
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-07-10 23:47:04 +0200

    fix: removed log

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,6 +2,6 @@ class HealthController < ApplicationController
   skip_before_action :authenticate_request
 
   def show
-    render json: { status: 'ok', secret_key_in_use: Rails.application.secret_key_base }, status: :ok
+    render json: { status: 'ok' }, status: :ok
   end
 end
