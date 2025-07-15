1bcab06f3347e4935dde57f3b8d14bb51e610fae
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-07-15 21:34:28 +0200

    fix: removed holding data from the mothod that saves the snapshot

--- app/services/portfolio_snapshot_service.rb
+++ app/services/portfolio_snapshot_service.rb
@@ -12,15 +12,15 @@ class PortfolioSnapshotService
   def snapshot
     holdings = @user.holdings
     return if holdings.empty?
-  
+
     labels = holdings.map(&:label).uniq
     prices_map = Price.where(label: labels).index_by(&:label)
-  
+
     latest_prices = holdings.map do |h|
       latest_price_record = prices_map[h.label]
-  
+
       next unless latest_price_record
-  
+
       {
         label: h.label,
         category: h.category,
@@ -29,11 +29,11 @@ class PortfolioSnapshotService
         value: (h.quantity * latest_price_record.price).to_f
       }
     end.compact
-  
+
     total_value = latest_prices.sum { |h| h[:value] }
-  
+
     @user.portfolio_snapshots.create!(
-      holdings_data: latest_prices,
+      # holdings_data: latest_prices,
       total_value: total_value,
       taken_at: Time.current
     )
